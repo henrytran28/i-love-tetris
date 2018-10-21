9de73040304ddaf327eeda4514a194353d4ee5de
Tai Lai <ctlai95@gmail.com>
2018-10-20 20:02:22 -0600

    Added test file for timer module

---
+++ src/timer/timer_test.lua
@@ -0,0 +1,39 @@
+
+local Timer = require("timer/timer")
+
+describe("#Timer", function() -- tagged as "Timer"
+    before_each(function()
+        timer = Timer:new()
+    end)
+
+    it("Constructor", function()
+        assert.are.equal(0, timer.value)
+    end)
+
+    it("Add and Subtract", function()
+        timer:add(1)
+        assert.are.equal(1, timer.value)
+        timer:add(1)
+        assert.are.equal(2, timer.value)
+        timer:subtract(2)
+        assert.are.equal(0, timer.value)
+    end)
+
+    it("Exceeds", function()
+        timer:add(1)
+        assert.is_true(timer:exceeds(0))
+        assert.is_false(timer:exceeds(2))
+    end)
+
+    it("Reset", function()
+        timer:add(1)
+        timer:reset()
+        assert.are.equal(0, timer.value)
+    end)
+
+    it("CalculateTime", function()
+        assert.are.equal(0, Timer.calculateTime(0, 1, 0))
+        assert.are.equal(1, Timer.calculateTime(0, 1, 100))
+        assert.are.equal(0.5, Timer.calculateTime(0, 1, 50))
+    end)
+end)
