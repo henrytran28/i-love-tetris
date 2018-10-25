8b8bf4ee03ceabd0f79054476cf278b25aaf5d3c
Tai Lai <ctlai95@gmail.com>
2018-10-25 00:09:32 -0600

    Reduced the handle repeat keys code

--- src/controller/controller.lua
+++ src/controller/controller.lua
@@ -7,60 +7,51 @@ local Controller = {}
 function Controller:new(board)
     local controller = {
         board = board,
-        leftTimer = Timer:new(),
-        rightTimer = Timer:new(),
-        downTimer = Timer:new(),
+        timers = {
+            ['moveLeft'] = Timer:new(),
+            ['moveRight'] = Timer:new(),
+            ['moveDown'] = Timer:new(),
+        },
     }
     self.__index = self
     setmetatable(controller, self)
     return controller
 end
 
+function Controller:run(action)
+    if action == 'moveLeft' then self.board.movement:moveLeft() end
+    if action == 'moveRight' then self.board.movement:moveRight() end
+    if action == 'moveDown' then self.board.movement:moveDown() end
+    if action == 'rotateCw' then self.board.movement:rotateCw() end
+    if action == 'rotateCcw' then self.board.movement:rotateCcw() end
+    if action == 'hardDrop' then self.board.movement:hardDrop() end
+    if action == 'hold' then self.board:holdCurrentTetromino() end
+end
+
 function Controller:handleNonRepeatKeys(key, scancode, isrepeat)
     for action, keyStrList in pairs(settings.keyBindings) do
         for _, keyStr in pairs(keyStrList) do
             if key == keyStr then
-                if action == 'moveLeft' then self.board.movement:moveLeft() end
-                if action == 'moveRight' then self.board.movement:moveRight() end
-                if action == 'moveDown' then self.board.movement:moveDown() end
-                if action == 'rotateCw' then self.board.movement:rotateCw() end
-                if action == 'rotateCcw' then self.board.movement:rotateCcw() end
-                if action == 'hardDrop' then self.board.movement:hardDrop() end
-                if action == 'hold' then self.board:holdCurrentTetromino() end
+                self:run(action)
             end
         end
     end
 end
 
 function Controller:handleRepeatKeys(dt)
-    if love.keyboard.isDown("left") then
-        self.leftTimer:add(dt)
-        if self.leftTimer:exceeds(utils.linearInterpolation(0.1, 0.3, settings.delayedAutoShiftPercent)) then
-            self.leftTimer:subtract(utils.linearInterpolation(0.1, 0.01, settings.leftRightSpeedPercent))
-            self.board.movement:moveLeft()
+    repeatActions = {'moveLeft', 'moveRight', 'moveDown'}
+    for _, action in pairs(repeatActions) do
+        for _, key in pairs(settings.keyBindings[action]) do
+            if love.keyboard.isDown(key) then
+                self.timers[action]:add(dt)
+                if self.timers[action]:exceeds(utils.linearInterpolation(0.1, 0.3, settings.delayedAutoShiftPercent)) then
+                    self.timers[action]:subtract(utils.linearInterpolation(0.1, 0.01, settings.leftRightSpeedPercent))
+                    self:run(action)
+                end
+            else
+                self.timers[action]:reset()
+            end
         end
-    else
-        self.leftTimer:reset()
-    end
-
-    if love.keyboard.isDown("right") then
-        self.rightTimer:add(dt)
-        if self.rightTimer:exceeds(utils.linearInterpolation(0.1, 0.3, settings.delayedAutoShiftPercent)) then
-            self.rightTimer:subtract(utils.linearInterpolation(0.1, 0.01, settings.leftRightSpeedPercent))
-            self.board.movement:moveRight()
-        end
-    else
-        self.rightTimer:reset()
-    end
-
-    if love.keyboard.isDown("down") then
-        self.downTimer:add(dt)
-        if self.downTimer:exceeds(utils.linearInterpolation(0.1, 0.3, settings.delayedAutoShiftPercent)) then
-            self.downTimer:subtract(utils.linearInterpolation(0.1, 0.01, settings.softDropSpeedPercent))
-            self.board.movement:moveDown()
-        end
-    else
-        self.downTimer:reset()
     end
 end
 
