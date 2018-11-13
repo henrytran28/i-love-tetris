af49ce907d922e55f2ab3ab7c97dc5796f5b936c
Tai Lai <ctlai95@gmail.com>
2018-11-13 10:24:42 -0700

    Remove inspect module from test file

--- src/movement/movement_test.lua
+++ src/movement/movement_test.lua
@@ -4,7 +4,6 @@ local Tetromino = require("tetromino/tetromino")
 local properties = require("tetromino/properties")
 local Point = require("point/point")
 local Square = require("square/square")
-local inspect = require("inspect")
 
 describe("#Movement", function() -- tagged as "Movement"
     before_each(function()
